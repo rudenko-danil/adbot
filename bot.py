178d7a1f29568daf6248434c1b784f05e920eb0d
rudenko-danil <rudenko.danil.v@gmail.com>
2025-05-05 20:58:46 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("8195235631:AAHMcqOGwYQ-3z87-LwTCcw_HXicIgsNDz0")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Бот работает на Fly.io!")
